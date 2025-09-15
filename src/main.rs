232e52bb1a238965fc07a82879689ac163de0f0e
roofiahmadsidiq <roofiahmadsidiq@gmail.com>
2025-09-15 09:24:05 +0700

    learn shadowing

--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,17 @@ fn rust_variable(){
     let mut name ="Roofi Ahmad Sidiq";
      println!("Hello {}", name);
     name = "Fatah";
-    println!("Hello {}", name);
+
+    let age:i32 = 29;
+    println!("Hello {}, your age is {}", name, age);
+}
+
+#[test]
+fn shadowing(){
+    let random_var = "I am Roofi";
+    println!("{}",random_var);
+
+    let random_var = 999;
+    println!("{}",random_var);
+
 }
