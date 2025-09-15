6afc7327cf2405db586b623b48edba91bb16bdc3
roofiahmadsidiq <roofiahmadsidiq@gmail.com>
2025-09-15 09:17:08 +0700

    learn mutable variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[test]
+fn hello_test(){
+    println!("Hello Test")
+}
+
+#[test]
+fn rust_variable(){
+    let mut name ="Roofi Ahmad Sidiq";
+     println!("Hello {}", name);
+    name = "Fatah";
+    println!("Hello {}", name);
+}
